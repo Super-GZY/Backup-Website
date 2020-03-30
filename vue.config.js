853fafc5cdb587e00966df6a702cd4340e3dd672
Super-GZY <1184072847@qq.com>
2020-03-30 15:18:23 +0800

    backup

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,5 @@ module.exports = {
   publicPath:  './', // 基本路径
   outputDir: 'dist', // 输出文件目录
   lintOnSave: false, // eslint-loader 是否在保存的时候检查
-  devServer:{
-    host:'0.0.0.0',
-    port:8000,
-    https: false,
-    hotOnly: false,
-  }
 }
 
