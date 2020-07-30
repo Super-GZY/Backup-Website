4b17b6c2cade330fb1c990e6dfa23d0334bae0d5
Super-GZY <1184072847@qq.com>
2020-07-30 18:42:49 +0800

    增加底部链接

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,6 +2,8 @@
   <div class="contact">
     <h1>携手我们 共赢未来！</h1>
     <span>北京一大波网络科技有限公司</span>
+      <a href="https://api.kuaidi100.com/" style="color:#fff">快递100</a>
+
     <!-- <div class="content"> -->
     <!-- <p><strong>投资合作</strong>: bitvee@outlook.com <strong>联系邮箱</strong>: wangyijia_hi@126.com</p> -->
     <!-- </div> -->
